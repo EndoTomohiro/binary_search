60c3eaa712a7363e1a98b1accfc0df389330c748
EndoTomohiro <endo.t.al@m.titech.ac.jp>
2020-07-03 09:29:45 +0900

    revised

--- src/apple.c
+++ src/apple.c
@@ -3,15 +3,32 @@
 int n;
 int k;
 int A[100000];
+int B[100000];
 
+
+int p(int t){
+  int i;
+  int x = 0;
+  for(i = 0; i < n; i++){
+    x += (A[i] + t - 1) / t;
+  }
+  return(x);
+}
 
 int main(){
-  int i, lb, ub;
+  int i;
   scanf("%d%d", &n, &k);
+  int lb = 0;
+  int ub = 100000;
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
 
+  while(ub - lb > 1){
+    int mid = (lb + ub) / 2;
+    if(p(mid) <= k) ub = mid;
+    else lb = mid;
+  }
 
-  return 0;
+  printf("%d\n", ub);
 }
